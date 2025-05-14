e5c56175245ca8b8de2a9f25165a72bba9b31db6
haroon 7012 <70120821@student.uol.edu.pk>
2025-05-14 12:38:53 +0500

    Added edit and update post functionality

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    // here is the model for the post
+    // it is used to interact with the posts table in the database
+    // it is used to create, read, update and delete posts
+    // it is used to validate the data before saving it to the database
+    // it is used to define the relationships between the posts and other models
     use HasFactory;
 
     // ✅ Add this:
